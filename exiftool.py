052865b7b9d88b33ad9273f8231914a15f053d04
Will Kaufman <will@wjkaufman.com>
2021-12-29 16:11:14 -0800

    Add write_metadata method

--- exiftool.py
+++ exiftool.py
@@ -33,6 +33,20 @@ class ExifTool(object):
             output += os.read(fd, 4096).decode('utf-8')
         return output[:-len(self.sentinel)]
 
-    def get_metadata(self, *filenames):
+    def read_metadata(self, *filenames):
         return json.loads(self.execute("-G", "-j", "-n", *filenames))
-        return json.loads(self.execute("-G", "-j", "-n", *filenames))
+    
+    def read_time_metadata(self, *filenames):
+        return json.loads(self.execute(
+            '-G', '-j', '-n', '-time:all',
+            *filenames
+        ))
+    
+    def write_metadata(self, filename, tags):
+        '''
+        Args:
+            tags (dict): A dictionary where the keys are metadata tags
+                and the values are the values.
+        '''
+        args = [f'-{k}={tags[k]}' for k in tags]
+        self.execute(*args, filename)
